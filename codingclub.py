0b434cdb95549e67684b5e551f81f265e0d4828b
Kushagra Agarwal <72088481+codewithkushagra@users.noreply.github.com>
2021-02-08 17:34:29 +0530

    Syntax optimaization
    No need for print statement before input, input function in python takes message statement for input

--- codingclub.py
+++ codingclub.py
@@ -1,10 +1,9 @@
-print("Enter your age:")
-age=int(input())
+age=int(input("Enter your age:"))
 
 if age>=18:
     print("You according to EC are eligible to vote and your vote counts")
 
-elif 1<=age<18:
+else:
     print("You are a kid and are not eligible to vote.")
 
 
